292c086c7faaafdf2ee35f658bd8d87f35d9bc12
Neroxman <neroxg@gmail.com>
2020-12-08 16:00:48 +0100

    Flow with page objects

--- cypress/integration/pageObjects.js
+++ cypress/integration/pageObjects.js
@@ -1,4 +1,7 @@
+import { onDatePickerPage } from "../support/page_objects/datepickerPage"
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutPage"
 import { navigateTo } from "../support/page_objects/NavigationPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
 context('Test with Page Ocjects', () => {
 
@@ -12,5 +15,18 @@ context('Test with Page Ocjects', () => {
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
         navigateTo.tosterPage()
+    })
+
+    it('should sumbit Inline and Basic form and select tomorrow date in the calendar', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInLineFormWithNameAndEmail('Mateusz', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+        navigateTo.datepickerPage()
+        onDatePickerPage.selectCommonDatePickerDateFromToday(1)
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstAndLastName('Mateusz', 'Kowal')
+        onSmartTablePage.updateAgeByFirstName('Mateusz','24')
+        onSmartTablePage.deleteRowByIndex(1)
     })
 })
