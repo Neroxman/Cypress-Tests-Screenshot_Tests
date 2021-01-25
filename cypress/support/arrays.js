fad6bff0224a5be25b984702c0f3722d4e07eebc
Neroxman <mateusz.brzezicki@eventory.cc>
2021-01-25 18:52:49 +0100

    Screenshot Tests | links to array added

--- cypress/support/arrays.js
+++ cypress/support/arrays.js
@@ -1,6 +1,21 @@
 const pageLinks = [
     'http://localhost:4200/pages',
-    'http://localhost:4200/pages/layout/stepper'
+    'http://localhost:4200/pages/layout/stepper',
+    'http://localhost:4200/pages/layout/accordion',
+    'http://localhost:4200/pages/forms/layouts',
+    'http://localhost:4200/pages/forms/datepicker',
+    'http://localhost:4200/pages/modal-overlays/dialog',
+    'http://localhost:4200/pages/modal-overlays/window',
+    'http://localhost:4200/pages/modal-overlays/popover',
+    'http://localhost:4200/pages/modal-overlays/toastr',
+    'http://localhost:4200/pages/modal-overlays/tooltip',
+    'http://localhost:4200/pages/extra-components/calendar',
+    'http://localhost:4200/pages/tables/smart-table',
+    'http://localhost:4200/pages/tables/tree-grid',
+    'http://localhost:4200/auth/login',
+    'http://localhost:4200/auth/register',
+    'http://localhost:4200/auth/request-password',
+    'http://localhost:4200/auth/reset-password'
 ]
 
 module.exports.pageLinks = pageLinks
