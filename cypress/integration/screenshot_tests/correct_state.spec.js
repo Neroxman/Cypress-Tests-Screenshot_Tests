bc9a3dc56a1752598a6aa9da1bf7169801de1d13
Neroxman <mateusz.brzezicki@eventory.cc>
2021-01-25 13:26:33 +0100

    Screenshot Tests | correct_state.spec.js added

---
+++ cypress/integration/screenshot_tests/correct_state.spec.js
@@ -0,0 +1,23 @@
+import { onlyOn } from '@cypress/skip-test'
+//import { marketingPageLinks, adminPanelPageLinks } from '../../../support/arrays.js'
+import { format } from 'date-fns'
+
+const day = format(new Date(), "d")
+let start_test = true
+
+if (day == '30') {
+  start_test = true
+}
+
+onlyOn(start_test === true, () => {
+  context('Correct screenshots', () => {
+    before(() => {
+      cy.viewport(1920, 1024)
+        .visit('http://localhost:4200/pages')
+    })
+
+    it('Correct state page', () => {
+      cy.correctScreenshot(marketingPageLinks)
+    })
+  })
+})
